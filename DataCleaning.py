b09d7d67a84a34dbb60a429d782c72d24103889b
andrinekirstine <andrine.kirstine@gmail.com>
2023-09-30 15:27:58 +0200

    feat(#3): create files and functions returing null

---
+++ DataCleaning.py
@@ -0,0 +1,23 @@
+def remove_lines():
+    # Take in folder with files and lines to remove
+    # Removes desired lines
+    # Return cleaned files
+    return null
+
+def remove_columns():
+    # Take in folder with files and undesired columns 
+    # Removes columns 
+    # Return changed files
+    return null
+
+def field_merger():
+    # Take in folder with files and fields to be merged
+    # Merge fields
+    # Return changed files
+    return null
+
+def field_converter():
+    # Take in folder with files and field to be converted and to what
+    # Convert field to wished type
+    # Return changed files
+    return null
